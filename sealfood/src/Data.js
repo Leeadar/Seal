ee2f58fd849c96c79ac4738c420a6db01b4b5335
eyalbab <eyalbahar@gmail.com>
2021-05-06 11:58:34 +0300

    added DataJs

---
+++ sealfood/src/Data.js
@@ -0,0 +1,112 @@
+const data = [
+    {
+        id: 1,
+        type: "meat",
+        name: "עוף בתיבול פיקנטי",
+        imgURL: "https://www.10dakot.co.il/wp-content/uploads/2021/02/%E2%80%8F%E2%80%8FDSC_0039-%D7%A2%D7%95%D7%AA%D7%A7.jpg",
+        recipe: ["מערבבים בקערית את חומרי התיבול ומתבלים את חלקי העוף, תפוחי האדמה והבטטה.",
+            "מניחים בתבנית ענקית מרופדת בנייר אפיה ומוסיפים שליש כוס מים.",
+            "מכניסים לתנור שחומם ל-185 מעלות במשך כשעה וחצי (חום עליון וחום תחתון)."],
+
+        products: ["8 שוקי עוף",
+            "2 ,תפוחי אדמה",
+            "בטטה קטנה קלופה"],
+        makingTime: "100 דקות"
+    },
+
+    {
+        id: 2,
+        type: "meat",
+        name: "נתחי אנטריקוט מוקפצים",
+        imgURL: "https://www.10dakot.co.il/wp-content/uploads/2019/03/%E2%80%8F%E2%80%8FDSC_0042-%D7%A2%D7%95%D7%AA%D7%A7-1.jpg",
+        recipe: ["מערבבים בקערית את חומרי התיבול ומתבלים את חלקי העוף, תפוחי האדמה והבטטה.",
+            "מניחים בתבנית ענקית מרופדת בנייר אפיה ומוסיפים שליש כוס מים.",
+            "מכניסים לתנור שחומם ל-185 מעלות במשך כשעה וחצי (חום עליון וחום תחתון)."],
+        products: [],
+        makingTime: "25 דקות"
+    },
+
+    {
+        id: 3,
+        type: "meat",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+
+    {
+        id: 4,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+    {
+        id: 5,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+    {
+        id: 6,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+    {
+        id: 7,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+    {
+        id: 8,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+    {
+        id: 9,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+    {
+        id: 10,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+
+    {
+        id: 11,
+        type: "",
+        name: "",
+        imgURL: "",
+        recipe: "",
+        products: [],
+        makingTime: ""
+    },
+]
